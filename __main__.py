e4ab289042d391c7117ebb3851f9350171cfc13d
Elan-R <63056651+Elan-R@users.noreply.github.com>
2020-07-08 14:29:53 -0400

    changed manifest.json to a python file

--- __main__.py
+++ __main__.py
@@ -1,14 +1,11 @@
 from discord.ext import commands
 import discord
+import manifest
 import ctypes
-import json
-
-with open("manifest.json", "r") as f:
-    manifest = json.load(f)
 
 ctypes.windll.kernel32.SetConsoleTitleW(manifest["terminal name"])
 
-bot = commands.Bot(command_prefix = manifest["prefix"])
+bot = commands.Bot(command_prefix = manifest.prefix])
 
 bot.remove_command("help")
 [bot.load_extension("Extensions." + extension) for extension in ["info", "credits", "gaming"]]
@@ -16,6 +13,6 @@ bot.remove_command("help")
 @bot.event
 async def on_ready():
     await bot.change_presence(status = discord.Status.online, activity = discord.Activity(type = discord.ActivityType.listening, name = "$info"))
-    print(manifest["ready message"])
+    print(manifest.ready_message])
 
-bot.run(manifest["token"])
+bot.run(manifest.token])
